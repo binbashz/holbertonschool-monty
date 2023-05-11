902d2e8a1b4b8ee25453a9d935feb6e75bc82ece
binbashz <5732@holbertonstudents.com>
2023-05-11 12:43:19 -0700

    erors

--- main.c
+++ main.c
@@ -1,19 +1,28 @@
 #include "monty.h"
-                                        
+                                          
 comandos *head = NULL;
 
-
+/**
+ * checking_blank - checks for blank spaces
+ * @s: source string
+ * Return: 0 if successful, 1 otherwise
+ */
 int checking_blank(char *s)
 {
 	size_t i = 0;
 
-	for (; s[i] && (s[i] == ' '); i++)		
-		
-	if (s[i] == EOF)
+	for (; s[i] && (s[i] == ' ' || s[i] == '\t'); i++)
+		;
+	if (s[i] == '\0')
 		return (0);
 	return (1);
 }
-
+/**
+ * main - entry point, evaluates path name.
+ * @argc: number of arguments.
+ * @argv: array of arguments.
+ * Return: EXIT_SUCCESS, EXIT_FAILURE.
+ */
 int main(int argc, char **argv)
 {
 	FILE *f;
@@ -34,11 +43,21 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	
+	for (i = 1; (r = getline(&s, &n, f)) != EOF; i++)
+	{
+		s[r - 1] = '\0';
+		if (!checking_blank(s) || hash_finder(&s) || !(*s))
+			continue;
 		
+	}
 	free(s), fclose(f);
-	
-	
-	
+	for (; head; free(temp2->command_argument[1]), free(temp2->command_argument[0]), free(temp2))
+	{
+		temp2 = head;
+		select_ops(&stk);
+		head = head->next;
+	}
+	free_all(&stk);
 	return (EXIT_SUCCESS);
 }
+
